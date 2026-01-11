2b76a25101a0c2abeed0ade95f9e61864f810312
Tarun <tarunpandore8@gmail.com>
2026-01-11 22:03:12 +0530

    fix(api): fix api config on the frontend

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
+const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
+console.log(baseURL)
 
 export const api = axios.create({
     baseURL,
