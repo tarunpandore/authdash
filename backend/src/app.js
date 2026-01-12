5a4f0be69ab508c0ce031a465fc45f61661d62d4
Tarun <tarunpandore8@gmail.com>
2026-01-12 10:22:16 +0530

    fix(backend): updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,34 +12,14 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://authdash-1ox4.onrender.com',
-    'https://authdash-tau.vercel.app'
-]
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        // allow requests with no origin like mobile apps or curl
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeader: ['Content-Type', 'Authorization'],
-    credentials: true,
-}
 
 // Connect to database
 connectDB();
 
 // Middleware
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
-app.options(/(.*)/, cors(corsOptions))
 
 // Auth routes
 app.use('/api/auth', authRoutes);
--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 console.log(baseURL)
 
 export const api = axios.create({
